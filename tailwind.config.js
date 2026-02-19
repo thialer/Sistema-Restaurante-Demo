911872708c34788a93a79799eb9ed0f5a55b8d25
Thialer <thialercamargo7@gmail.com>
2026-02-18 21:10:47 -0300

    Corrigindo background direto no HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./*.html", "./script.js"],
   theme: {
     extend: {
-      backgroundImage: {
-        home: "url('/Sistema-Restaurante-Demo/assets/bg.png')",
-      },
+
     },
   },
   plugins: [],
